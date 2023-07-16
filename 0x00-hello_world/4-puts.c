a922ad3eecb35c620b9d3842d6f6bdd7249f48f7
abehilmar <belobehilmar@gmail.com>
2023-07-16 22:44:11 +0300

    fix betty bullshite

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- *main - function
- *Return : success
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
