4d818ccafe55f1c36b102c7377b84502911d0a50
abehilmar <belobehilmar@gmail.com>
2023-07-18 00:17:06 +0300

    task 5

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <>
+/**
+ * main- the fun
+ * Return: success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n + 48);
+	}
+	putchar('\n');
+	return (0);
+}
