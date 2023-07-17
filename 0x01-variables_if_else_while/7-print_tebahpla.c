db041a98edcf5db28450020ff26d13546006d562
abehilmar <belobehilmar@gmail.com>
2023-07-18 00:21:45 +0300

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main- function
+ *Return: always true
+ */
+int main(void)
+{
+	int n = 122;
+
+	while (n >= 97)
+	{
+		putchar(n);
+		n--;
+	}
+	putchar('\n');
+	return (0);
+}
