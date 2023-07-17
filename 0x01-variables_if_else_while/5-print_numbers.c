2704f71652fb1a8fddcdd899643f400fc263ecab
abehilmar <belobehilmar@gmail.com>
2023-07-18 00:11:29 +0300

    task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
- * main - the function
- * Return : success always
+ *main - the function
+ * Return: success always
  */
 int main(void)
 {
 	int n;
 
-	for(n=0: n <=9; n++)
+	for (n = 0; n <= 9; n++)
 	{
-		printf("%d",n);
+		printf("%d", n);
 	}
 	printf("\n");
 	return (0);
