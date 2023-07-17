e7f8d5d35edc61bf8d32e04ccf08168f7ba400e4
abehilmar <belobehilmar@gmail.com>
2023-07-17 23:01:21 +0300

    print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - functin goes here
+ *Description : print alpapets
+ *Return: Always 0
+ */
+int main(void)
+{
+	int init = 97;
+
+	while (init <= 122)
+	{
+		putchar (init);
+		init++;
+	}
+	return (0);
+}
