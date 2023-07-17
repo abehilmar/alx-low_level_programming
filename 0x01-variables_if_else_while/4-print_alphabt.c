1039c7cf330b05208c97ac73756230c3389fa73c
abehilmar <belobehilmar@gmail.com>
2023-07-17 23:20:05 +0300

    4 print alphabt with out E and Q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - functin goes here
+ *Description : print alpapets
+ *Return: Always 0
+ */
+int main(void)
+{
+	int init = 97;
+
+	while (init <= 122)
+	{
+		if (init != 65 || init != 71)
+		{
+			putchar (init);
+			init++;
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
