74123d1e8596370c10442308bdc3128cfb8a9bc6
abehilmar <belobehilmar@gmail.com>
2023-07-27 22:25:06 +0300

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_abs - function
+ *
+ *@i: input
+ *Return: absolute value of i
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
+
