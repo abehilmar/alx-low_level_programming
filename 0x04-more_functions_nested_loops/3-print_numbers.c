6c63b2e35334a39ce7d5c71762bbed64689efe5c
abehilmar <belobehilmar@gmail.com>
2023-07-28 13:21:45 +0300

    the number speak for themselves task done with its main and tes files

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_numbers - function
+ */
+void print_numbers(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		_putchar('0' + num);
+		num++;
+	}
+	_putchar('\n');
+}
