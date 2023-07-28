97e6f84a981b27e608bc35ef36ef2164c8e1b27e
abehilmar <belobehilmar@gmail.com>
2023-07-28 12:24:20 +0300

    is digit task done

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,29 +1,15 @@
 #include "main.h"
-
 /**
-
- * _isupper - Check  given char is an uppercase letter.
-
- * @c: input char
-
- *
-
- * Return: 1 if upper else 0 (False)
-
+ * _isdigit - function.
+ *@c: input
+ *Return: 1 if true else 0 (False)
  */
-
-int _isupper(int c)
-
+int _isdigit(int c)
 {
-
-	if (c >= 65 && c <= 90)
-
+	if (c >= '0' && c <= '9')
 	{
-
 		return (1);
-
 	}
-
 	return (0);
-
 }
+
