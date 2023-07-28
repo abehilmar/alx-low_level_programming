e72441792fcbac256289fc062a7559c635f6dc96
abehilmar <belobehilmar@gmail.com>
2023-07-28 12:15:53 +0300

    isupper fix betty std

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,29 +1,17 @@
 #include "main.h"
 
 /**
-
  * _isupper - Check  given char is an uppercase letter.
-
  * @c: input char
-
  *
-
  * Return: 1 if upper else 0 (False)
-
  */
 
 int _isupper(int c)
-
 {
-
 	if (c >= 65 && c <= 90)
-
 	{
-
 		return (1);
-
 	}
-
 	return (0);
-
 }
