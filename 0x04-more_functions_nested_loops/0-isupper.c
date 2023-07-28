cf809ffb7f0d216eff79bd002f3db0d52812f89e
abehilmar <belobehilmar@gmail.com>
2023-07-28 12:14:07 +0300

    is upper file with its test file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+
+ * _isupper - Check  given char is an uppercase letter.
+
+ * @c: input char
+
+ *
+
+ * Return: 1 if upper else 0 (False)
+
+ */
+
+int _isupper(int c)
+
+{
+
+	if (c >= 65 && c <= 90)
+
+	{
+
+		return (1);
+
+	}
+
+	return (0);
+
+}
