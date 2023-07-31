38499965b40b03113cb1aa8058d346ab3e4860b6
abehilmar <belobehilmar@gmail.com>
2023-07-31 22:24:32 +0300

    task 4 test file

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *_strpbrk - function
+ *@s: first value
+ *@accept: second value
+ *Return: char with result 
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int j = 0;
+
+	while (*s)
+	{
+		while (accept[j] != '\0')
+		{
+			if (*s == accept[j])
+				return (s);
+			j++;
+		}
+		j = 0;
+		s++;
+	}
+	return ('\0');
+}
